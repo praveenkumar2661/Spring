8299eee1eb5b36d9d885e6508d97b4bf36f2994c
praveenkumar2661 <praveenkumar1856399@gmail.com>
2024-02-19 16:45:21 +0530

    Bean Property

--- SpringFramework/src/main/java/spring/App.java
+++ SpringFramework/src/main/java/spring/App.java
@@ -8,7 +8,9 @@ public class App
 	public static void main(String[] args) 
 	{
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		Vehicle obj = (Vehicle)context.getBean("car");
-		obj.drive();
+//		Vehicle obj = (Vehicle)context.getBean("car");
+//		obj.drive();
+		Tyre t = (Tyre)context.getBean("tyre");
+		System.out.println(t);
 	}
 }
---
+++ SpringFramework/src/main/java/spring/Tyre.java
@@ -0,0 +1,31 @@
+package spring;
+
+public class Tyre
+{
+	private String brand;
+	private int inch;
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getInch() {
+		return inch;
+	}
+
+	public void setInch(int inch) {
+		this.inch = inch;
+	}
+
+	@Override
+	public String toString() {
+		return "Tyre [brand=" + brand + ", inch=" + inch + "]";
+	}
+
+	
+	
+}
