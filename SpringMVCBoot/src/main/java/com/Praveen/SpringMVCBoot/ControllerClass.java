daed641b17dd03269e40204626c492a803356fdd
praveenkumar2661 <praveenkumar1856399@gmail.com>
2024-02-21 22:03:30 +0530

    Prefix and Suufix

--- SpringMVCBoot/src/main/java/com/Praveen/SpringMVCBoot/ControllerClass.java
+++ SpringMVCBoot/src/main/java/com/Praveen/SpringMVCBoot/ControllerClass.java
@@ -15,13 +15,13 @@ public class ControllerClass
 	@RequestMapping("/")
 	public String home()
 	{
-		return "index.jsp";
+		return "index";
 	}
 	@RequestMapping("add")
 	public ModelAndView add(@RequestParam("num1") int i,@RequestParam("num2") int j)
 	{
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("result.jsp");
+		mv.setViewName("result");
 		mv.addObject("result",i+j);
 		return mv;
 	}
